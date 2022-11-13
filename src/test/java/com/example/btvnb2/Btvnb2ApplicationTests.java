93500d9dd53c255406ecd1fa394a2df26e734fee
dung <vdung5673@gmail.com>
2022-11-13 14:37:23 +0700

    dung

---
+++ src/test/java/com/example/btvnb2/Btvnb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btvnb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Btvnb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
